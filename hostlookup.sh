307b0365ff2333cd95138ac4b1c940b4f060d815
Brandon Boras <brandon.boras@mymail.champlain.edu>
2020-10-16 01:17:24 -0400

    hostlookup.sh is fully implemented

--- hostlookup.sh
+++ hostlookup.sh
@@ -5,8 +5,32 @@
 # author brandon.boras
 # date 10/15/2020
 
-for h in champlain.edu uvm.edu umd.edu doesnotexist.edu
-do
-	host -W1 -t A $h
-done
+if [ $@ ]; then
+	if [[ -f "$1" ]]; then
+		all_invalid=true
+		cat ${1} | while read line
+		do
+			out=$(host -W1 -t A $line)
+			if [ $? -eq 0 ];then
+				ip=$(echo $out | cut -d " " -f 4)
+				echo ${line},$ip
+				all_invalid=false
+			else
+				echo "${line},not found"
+			fi
+		done
+		
+		if $all_valid; then
+			echo "None of the hosts were valid"
+			exit 1
+		fi
+	else
+		echo "That was not a file"
+		exit 1
+	fi
+else
+	echo "Nothing was passed!"
+	exit 1
+fi
+
 
